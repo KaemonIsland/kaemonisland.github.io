f3bf0b917f5f0a3a08e036d3fc0011f9a82241b5
Kaemon Lovendahl <k-manskrazy@hotmail.com>
2019-11-25 20:55:46 -0700

    Add Flex Item

--- src/pages/characterSheet/Spells.jsx
+++ src/pages/characterSheet/Spells.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Container } from '../../layout'
+import { Container, Flex } from '../../layout'
 
 const SpellsContainer = styled.div`
   color: blue;
@@ -10,8 +10,18 @@ export const Spells = () => {
   const name = 'Spells'
   return (
     <SpellsContainer>
-      <Container border width="20%">
-        <h1>{name}</h1>
+      <Container border height="50vh">
+        <Flex justifyContent="space-evenly" alignItems="center">
+          <Flex.Item>
+            <h4>{name}</h4>
+          </Flex.Item>
+          <Flex.Item>
+            <h4>{name}</h4>
+          </Flex.Item>
+          <Flex.Item>
+            <h4>{name}</h4>
+          </Flex.Item>
+        </Flex>
       </Container>
     </SpellsContainer>
   )
